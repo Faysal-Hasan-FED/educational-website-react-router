6c5b5e6a77d044a9395f7bff4230cd5e045e6147
Faysal <frontendfaysal@gmail.com>
2022-04-09 00:42:01 +0600

    added image in not found

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import img from '../../images/notFound.jpg';
 
+
 const NotFound = () => {
+    const navigate = useNavigate();
+
+    const handleButton = () =>{
+    navigate('/home');
+    }
     return (
-        <div>
+        <div className='text-center'>
+            <div className='my-5'>
             <img src={img} alt="" />
+            </div>
+            <Button onClick={handleButton} variant='outline-dark'>Go Home</Button>
         </div>
     );
 };
